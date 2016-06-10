0484736a42dc3c705648cad1e37132af91c82e3f
Matt Stevens <matt@stevens.net.nz>
2016-06-10 14:41:48 +1200

    save me

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,7 @@
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello dogs");
+        System.out.println("Hello pooch");
     }
 
 }
